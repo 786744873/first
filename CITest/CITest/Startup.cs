76a713b34fc347b63ada6b450302d6e0ef815d08
玉廷 王 <786744873@qq.com>
2019-05-02 22:16:43 +0800

    提交修改

--- CITest/CITest/Startup.cs
+++ CITest/CITest/Startup.cs
@@ -23,7 +23,8 @@ namespace CITest
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc();
+            //
+            services.AddMvc();   
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
